7e6eb0bcd606601f289b102fca7ac3d316c115d8
enachb <erich.nachbar@gmail.com>
2020-01-13 21:41:11 -0800

    added BLE scanning. not tested yet due to lack of BLE adapter at the time

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.13
 require (
 	github.com/boombuler/barcode v1.0.0
 	github.com/jung-kurt/gofpdf v1.16.2
+	github.com/paypal/gatt v0.0.0-20151011220935-4ae819d591cf
+	github.com/sirupsen/logrus v1.4.2
 )
--- main.go
+++ main.go
@@ -1,16 +1,107 @@
 package main
 
 import (
-	"fmt"
 	"image/jpeg"
+	"io/ioutil"
 	"os"
 	"strings"
+	"sync"
+	"time"
+
+	"github.com/jung-kurt/gofpdf"
+	log "github.com/sirupsen/logrus"
+
+	stdlog "log"
+
+	"github.com/paypal/gatt"
+	"github.com/paypal/gatt/examples/option"
 
 	"github.com/boombuler/barcode"
 	bc "github.com/boombuler/barcode/code39"
 	"github.com/boombuler/barcode/qr"
-	"github.com/jung-kurt/gofpdf"
 )
+
+var isPoweredOn = false
+var scanMutex = sync.Mutex{}
+
+func beginScan(d gatt.Device) {
+	scanMutex.Lock()
+	for isPoweredOn {
+		d.Scan(nil, true) //Scan for five seconds and then restart
+		time.Sleep(5 * time.Second)
+		d.StopScanning()
+	}
+	scanMutex.Unlock()
+}
+
+func onStateChanged(d gatt.Device, s gatt.State) {
+	log.Debugf("State:", s)
+	switch s {
+	case gatt.StatePoweredOn:
+		log.Debug("scanning...")
+		isPoweredOn = true
+		go beginScan(d)
+		return
+	case gatt.StatePoweredOff:
+		log.Debug("REINIT ON POWER OFF")
+		isPoweredOn = false
+		d.Init(onStateChanged)
+	default:
+		log.Debugf("WARN: unhandled state: ", string(s))
+	}
+}
+
+func onPeriphDiscovered(p gatt.Peripheral, a *gatt.Advertisement, rssi int) {
+	log.Debugf("Peripheral ID:%s, NAME:(%s) power (%d)", p.ID(), p.Name(), a.TxPowerLevel)
+	if rssi < -30 {
+
+		text := strings.Replace(p.ID(), ":", "", -1)
+		log.Infof("Found new beacon: %s rssi:%d", p.ID(), rssi)
+
+		filenameBC := makeCode39(text)
+		filenameQR := makeQR(text)
+
+		pdf := gofpdf.NewCustom(
+			&gofpdf.InitType{
+				UnitStr:        "mm",
+				Size:           gofpdf.SizeType{Wd: 12, Ht: 80},
+				FontDirStr:     "",
+				OrientationStr: "L",
+			})
+		//	pdf.AddPage()
+		pdf.SetFont("Arial", "B", 8)
+
+		// CellFormat(width, height, text, border, position after, align, fill, link, linkStr)
+		pdf.CellFormat(0, 0, text, "0", 0, "LB", false, 0, "")
+
+		// ImageOptions(src, x, y, width, height, flow, options, link, linkStr)
+		pdf.ImageOptions(
+			filenameBC,
+			1, 1,
+			35, 5,
+			false,
+			gofpdf.ImageOptions{ImageType: "JPG", ReadDpi: true},
+			0,
+			"",
+		)
+
+		pdf.ImageOptions(
+			filenameQR,
+			70, 1,
+			10, 10,
+			false,
+			gofpdf.ImageOptions{ImageType: "JPG", ReadDpi: true},
+			0,
+			"",
+		)
+
+		err := pdf.OutputFileAndClose("oink.pdf")
+		log.Info("Done printing barcode")
+		if err != nil {
+			panic(err)
+		}
+	}
+}
 
 func makeCode39(text string) string {
 	filename := "barcode.jpg"
@@ -68,48 +159,31 @@ func makeQR(text string) string {
 
 func main() {
 
-	text := strings.Replace("AA:FF:DD:99", ":", "", -1)
-	fmt.Printf("Text %s\n", text)
+	// Supress extraneous log output from GATT
+	stdlog.SetOutput(ioutil.Discard)
 
-	filenameBC := makeCode39(text)
-	filenameQR := makeQR(text)
+	//dataChan = make(chan string, 1)
 
-	pdf := gofpdf.NewCustom(
-		&gofpdf.InitType{
-			UnitStr:        "mm",
-			Size:           gofpdf.SizeType{Wd: 12, Ht: 80},
-			FontDirStr:     "",
-			OrientationStr: "L",
-		})
-	//	pdf.AddPage()
-	pdf.SetFont("Arial", "B", 8)
+	log.SetFormatter(&log.TextFormatter{
+		DisableColors: false,
+		FullTimestamp: true,
+	})
 
-	// CellFormat(width, height, text, border, position after, align, fill, link, linkStr)
-	pdf.CellFormat(0, 0, text, "0", 0, "LB", false, 0, "")
+	log.SetFormatter(&log.TextFormatter{})
 
-	// ImageOptions(src, x, y, width, height, flow, options, link, linkStr)
-	pdf.ImageOptions(
-		filenameBC,
-		1, 1,
-		35, 5,
-		false,
-		gofpdf.ImageOptions{ImageType: "JPG", ReadDpi: true},
-		0,
-		"",
-	)
-
-	pdf.ImageOptions(
-		filenameQR,
-		70, 1,
-		10, 10,
-		false,
-		gofpdf.ImageOptions{ImageType: "JPG", ReadDpi: true},
-		0,
-		"",
-	)
-
-	err := pdf.OutputFileAndClose("oink.pdf")
+	d, err := gatt.NewDevice(option.DefaultClientOptions...)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Failed to open device, err: %s\n", err)
+		return
 	}
+
+	// Register handlers.
+	d.Handle(gatt.PeripheralDiscovered(onPeriphDiscovered))
+	d.Init(onStateChanged)
+
+	//	text := strings.Replace("AA:FF:DD:99", ":", "", -1)
+	//fmt.Printf("Text %s\n", text)
+
+	// waiting
+	select {}
 }
